c6f81b1581c70a592cf69edee56a291b1112310d
raphael86 <raphael86.shin@gamil.com>
2016-10-15 15:08:09 +0900

    day1 파이썬 코드 실습

---
+++ day1/turtle5.py
@@ -0,0 +1,7 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.speed(2)
+
+turtle.circle(100)
+
---
+++ day1/turtle6.py
@@ -0,0 +1,77 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.speed(3)
+
+turtle.circle(100)
+
+turtle.right(90)
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+turtle.left(90)
+
+turtle.circle(150)
+
+turtle.left(90)
+turtle.penup()
+turtle.forward(150)
+turtle.pendown()
+
+# 삼각형 1번째 그리기
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+
+turtle.right(60)
+
+# 삼각형 2번째 그리기
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+
+turtle.right(60)
+
+# 삼각형 3번째 그리기
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+
+turtle.right(60)
+
+# 삼각형 4번째 그리기
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+
+turtle.right(60)
+
+# 삼각형 5번째 그리기
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+
+turtle.right(60)
+
+# 삼각형 6번째 그리기
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
